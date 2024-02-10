1b3e2f0d3ef0fd059b0447f01a313409d72aa636
Bernd Janzen <103428887+manjou@users.noreply.github.com>
2024-02-10 10:29:13 +0100

    Exercise 5.2. v.1.2.0

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 // import the screens
 import Start from './components/Start';
 import Chat from './components/Chat';
+
 // import the necessary components from react-native
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
-
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+//importing Initailization for the Firebase
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDkd3DVIg-By9zupOJ5U1T9PBZL_UDSOM0",
+    authDomain: "shopping-list-demo-5a740.firebaseapp.com",
+    projectId: "shopping-list-demo-5a740",
+    storageBucket: "shopping-list-demo-5a740.appspot.com",
+    messagingSenderId: "550250686696",
+    appId: "1:550250686696:web:a7ea55253a87defe1c015d",
+    measurementId: "G-D6XY59CEKM"
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Start'>
@@ -32,3 +51,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
